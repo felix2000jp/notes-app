8fb8210834090cc8b1ff46e30ab9ba6ee6f4d7f8
felix2000jp <felix2000jp@gmail.com>
2022-12-12 20:30:41 +0000

    updated api client

--- frontend/src/Services/ApiClient.ts
+++ frontend/src/Services/ApiClient.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const ApiClient = axios.create({ baseURL: "https://notes-web-app-production.up.railway.app" });
+const ApiClient = axios.create({ baseURL: "https://notes-web-app-backend-production.up.railway.app" });
 export default ApiClient;
